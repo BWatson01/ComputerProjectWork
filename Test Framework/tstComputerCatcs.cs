7062c84366bed4d283e510184b7d2541ba0ddbf8
p17218124 <p17218124@my365.dmu.ac.uk>
2019-02-06 12:08:09 +0000

    some changes

---
+++ Test Framework/tstComputerCatcs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Class_Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstComputerCatcs
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of ur class clsComputer
+            clsComputerCat AnComputerCat = new clsComputerCat();
+            //check to see that the class is not bull
+            Assert.IsNotNull(AnComputerCat);
+        }
+    }
+}
