bb235a98cdd1fa5200ae98f7d768aed4d5c8f5ac
p1718985x <p1718985x@GWHXX653WIN014W>
2019-02-06 12:31:17 +0000

    Created tstOrder and did the first test, InstanceOK on clsOrder

---
+++ Test Framework/tstOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Class_Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create n instnce of our class clsOrder
+            clsOrder AnOrder = new clsOrder();
+            //check to se that the class is not null
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
