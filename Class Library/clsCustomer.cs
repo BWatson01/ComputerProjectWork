b79fe02ecaf1966d03e846837181cfe9009378dd
p17190399 <p17190399@GWHXX282WIN019W>
2019-02-13 11:43:56 +0000

    made chnges

---
+++ Class Library/clsCustomer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Class_Library
+{
+    public class clsCustomer
+    {
+        public string CustomerName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string CustomerAddress { get; set; }
+        public int CustomerNo { get; set; }
+        public string CustomerPostCode { get; set; }
+
+        public string Valid(string someCustomer)
+        {
+            //string variable to store the error message
+            string Error = "";
+            //if the name of the county is not blank
+            if (someCustomer.Length > 50)
+            {
+                //return an error message
+                Error = "The Customer Name cannot have more than 50 characters";
+            }
+            if (someCustomer.Length == 0)
+            {
+                //return an error message
+                Error = "The Customer Name may not be blank!";
+            }
+            return Error;
+        }
+    }
+}
