19eb510cdbc3972f5f57aecb9aef9cfdc1a281bd
p1718985x <p1718985x@GWHXX653WIN014W>
2019-02-13 11:03:59 +0000

    Made more changes to my class clsOrder

--- Class Library/clsOrder.cs
+++ Class Library/clsOrder.cs
@@ -1,6 +1,31 @@
-﻿namespace Class_Library
+﻿using System;
+
+namespace Class_Library
 {
     public class clsOrder
     {
+        public int OrderNo { get; set; }
+        public int ComputerNo { get; set; }
+        public int CustomerNo { get; set; }
+        public string ItemName { get; set; }
+        public int Invoice { get; set; }
+
+        public string Valid(string someOrder)
+        {
+            //string variable to store the error message
+            string Error = "";
+            //if the name of the county is not blank
+            if (someOrder.Length > 50)
+            {
+                //return an error message
+                Error = "The characters cannot be more than 50 characters";
+            }
+            if (someOrder.Length == 0)
+            {
+                //return an error message
+                Error = "The section may not be blank!";
+            }
+            return Error;
+        }
     }
 }
--- Test Framework/tstOrder.cs
+++ Test Framework/tstOrder.cs
@@ -15,5 +15,91 @@ namespace Test_Framework
             //check to se that the class is not null
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void OrderNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 SomeOrderNo = 1;
+            //assign the data to the property
+            AnOrder.OrderNo = SomeOrderNo;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.OrderNo, SomeOrderNo);
+        }
+
+
+        [TestMethod]
+        public void ComputerNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 SomeComputerNo = 1;
+            //assign the data to the property
+            AnOrder.ComputerNo = SomeComputerNo;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.ComputerNo, SomeComputerNo);
+        }
+
+
+        [TestMethod]
+        public void CustomerNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 SomeCustomerNo = 1;
+            //assign the data to the property
+            AnOrder.CustomerNo = SomeCustomerNo;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.CustomerNo, SomeCustomerNo);
+        }
+
+
+        [TestMethod]
+        public void ItemNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            string SomeItem = "Asus650";
+            //assign the data to the property
+            AnOrder.ItemName = SomeItem;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.ItemName, SomeItem);
+        }
+
+        [TestMethod]
+        public void InvoicePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 SomeInvoice = 635;
+            //assign the data to the property
+            AnOrder.Invoice = SomeInvoice;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.Invoice, SomeInvoice);
+        }
+
+
+
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create a string variable to store the results of the method
+            String Error = "";
+            //create some test data to test the method
+            string SomeItem = "Asus650";
+            //invoke the method
+            Error = AnOrder.Valid(SomeItem);
+            //test to see that the result is OK i.e there was no error message returned
+            Assert.AreEqual(Error, "");
+        }
+
     }
 }
