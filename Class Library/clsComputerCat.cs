f654c45ac247a1b99bf7d460ca670a20e7a8397b
p17218124 <p17218124@my365.dmu.ac.uk>
2019-03-06 11:02:37 +0000

    testing improved

--- Class Library/clsComputerCat.cs
+++ Class Library/clsComputerCat.cs
@@ -1,6 +1,114 @@
-﻿namespace Class_Library
+﻿using System;
+
+namespace Class_Library
 {
     public class clsComputerCat
     {
+        //private data member for the ComputerName property
+        private Int32 mComputerName;
+        //private data member for computerName
+        private int mComputerID;
+        //private int for mComputerID
+        private int mComputerSize;
+        //private data for ComputerSize
+        private string mComputerManufacturer;
+        //private data for ComputerManufacturer
+
+       
+        //public proeperty for the computer name
+        public int ComputerName
+        {
+            get
+            {
+                //return the private data
+                return mComputerName;
+
+            }
+            set
+            {
+                //set the value of the private data member
+                mComputerName = value;
+            }
+        }
+        public int ComputerID
+        {
+            get
+            {
+                //return the private data
+                return mComputerID;
+
+            }
+            set
+            {
+                //set the value of the private data member
+                mComputerID = value;
+            }
+        }
+        public int ComputerSize
+        {
+            get
+            {
+                //return the private data
+                return mComputerSize;
+
+            }
+            set
+            {
+                //set the value of the private data member
+                mComputerSize = value;
+            }
+
+
+        }
+        public string ComputerManufacturer
+        {
+            get
+            {
+                //return the private data
+                return mComputerManufacturer;
+
+            }
+            set
+            {
+                //set the value of the private data member
+                mComputerManufacturer = value;
+            }
+
+        }
+
+
+
+
+        public bool Find(int ComputerID)
+        {
+            //create an instance of the data connection
+            clsDataCollection DB = new clsDataCollection();
+            //add the parameter for the address no to search for 
+            DB.addParameter("@ComputerNo", ComputerID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblComputer_FilterByComputerID");
+            //if one record is found (there should be either one or zero!)
+            if (DB.Count ==1)
+            {
+                //copy the data from the database to the private data members
+
+            }
+            mComputerID = Convert.ToInt32(DB.dataTable.Rows[0]["ComputerID"]);
+            mComputerName = Convert.ToInt32(DB.dataTable.Rows[0]["ComputerName"]);
+            mComputerID = Convert.ToString(DB.dataTable.Rows[0]["ComputerManufacturer"]);
+            mComputerSize = Convert.ToInt32(DB.dataTable.Rows[0]["ComputerSize"]);
+            //return always true
+            return true;
+        }
+        //if no record was found
+        else
+        {
+        //return false indicating a problem
+        return false;
+        }
+
+           
+           
+        }
     }
 }
--- Test Framework/tstComputerCatcs.cs
+++ Test Framework/tstComputerCatcs.cs
@@ -5,15 +5,180 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Test_Framework
 {
     [TestClass]
-    public class tstComputerCatcs
+    public class tstComputerCat
     {
         [TestMethod]
         public void InstanceOK()
         {
             //create an instance of ur class clsComputer
             clsComputerCat AnComputerCat = new clsComputerCat();
-            //check to see that the class is not bull
+            //test to see that it exists
             Assert.IsNotNull(AnComputerCat);
         }
+        [TestMethod]
+        public void ComputerIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsComputerCat AnComputerCat = new clsComputerCat();
+            //create some test data to assign to the property
+            int TestData = 211;
+            //assign the data to the property
+            AnComputerCat.ComputerID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnComputerCat.ComputerID, TestData);
+        }
+
+        [TestMethod]
+        public void ComputerNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsComputerCat AnComputerCat = new clsComputerCat();
+            //create some test data to assign to the property
+            Int32 TestData = 1011;
+            //assign the data to the property
+            AnComputerCat.ComputerName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnComputerCat.ComputerName, TestData);
+        }
+
+        [TestMethod]
+        public void ComputermanufacturerOK()
+        {
+            //create an instance of the class we want to create
+            clsComputerCat AnComputerCat = new clsComputerCat();
+            //create some test data to assign to the property
+            string Testdata = "HP";
+            //assign the data to the property
+            AnComputerCat.ComputerManufacturer = Testdata;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnComputerCat.ComputerID, Testdata);
+        }
+
+        [TestMethod]
+        public void ComputerSizeOK()
+        {
+            //create an instance of the class we want to create
+            clsComputerCat AnComputerCat = new clsComputerCat();
+            //create some test data to assign to the property
+            Int32 TestData = 3;
+            //assign the data to the property
+            AnComputerCat.ComputerSize = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnComputerCat.ComputerSize, TestData);
+        }
+
+        [TestMethod]
+
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+
+            clsComputerCat AnPc = new clsComputerCat();
+            //boolean variable to store the result of validation
+            Boolean Found = false;
+            //create test data with the method
+            Int32 ComputerID = 1;
+            //invoke the method
+            Found = AnPc.Find(ComputerID);
+            //test to see that the result is correct
+            Assert.IsTrue(Found);
+        }
+        [TestMethod]
+        public void TestComputerNameFound()
+        {
+            //create an instance of the class we want to create
+            clsComputerCat AnComputerCat = new clsComputerCat();
+            //boolEAN VARIABLE TO STORE THE result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is good
+            Boolean OK = true;
+            //create some test data to use with the method
+
+            Int32 ComputerID = 23;
+            //invoke the method
+
+            Found = AnComputerCat.Find(ComputerID);
+            //check the computer Name
+            if (AnComputerCat.ComputerName != 21)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestComputerManufacturerFound()
+        {
+            //create an instance of the class we want to create
+            clsComputerCat AnComputerCat = new clsComputerCat();
+            //boolEAN VARIABLE TO STORE THE result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is good
+            Boolean OK = true;
+            //create some test data to use with the method
+
+            Int32 ComputerID = 23;
+            //invoke the method
+
+            Found = AnComputerCat.Find(ComputerID);
+            //check the computer Name
+            if (AnComputerCat.ComputerManufacturer != "Test Manufacturer")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+
+        }
+        [TestMethod]
+        public void TestComputerSizeFound()
+        {
+            //create an instance of the class we want to create
+            clsComputerCat AnComputerCat = new clsComputerCat();
+            //boolEAN VARIABLE TO STORE THE result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is good
+            Boolean OK = true;
+            //create some test data to use with the method
+
+            Int32 ComputerID = 23;
+            //invoke the method
+
+            Found = AnComputerCat.Find(ComputerID);
+            //check the computer Name
+            if (AnComputerCat.ComputerSize != 21)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+
+        }
+
+
+        [TestMethod]
+        public void TestComputerIDFound()
+        {
+            //create an instance of the class we want to create
+            clsComputerCat AnComputerCat = new clsComputerCat();
+            //boolEAN VARIABLE TO STORE THE result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is good
+            Boolean OK = true;
+            //create some test data to use with the method
+
+            Int32 ComputerID = 211;
+            //invoke the method
+
+            Found = AnComputerCat.Find(ComputerID);
+            //check the computer Name
+            if (AnComputerCat.ComputerID != 322)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+        }
     }
-}
+
+  }
